a88411101b9e6baf4084dca564e41759f0eb1267
Dalbert Ang <da@architech.ca>
2025-02-26 10:03:22 -0500

    feat: initial

---
+++ src/main/java/com/dalbertang/live_chat_backend/LiveChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dalbertang.live_chat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalbertang/live_chat_backend/LiveChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalbertang.live_chat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
